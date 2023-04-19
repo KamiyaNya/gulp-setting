9fd0ccbef4e8ca2c76ccaf657fa004f26271066c
KamiyaNya <dikriss16@gmail.com>
2023-04-19 16:56:45 +0300

    Изменение структуры вложенности и изменен путь таски для pug

--- tasks/Pug.js
+++ tasks/Pug.js
@@ -1,12 +1,13 @@
-const gulp = require("gulp");
-const pug = require("gulp-pug");
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync');
+const excludePaths = ['!./src/pug/pages/**/components'];
 
 // * Задача для конвертации pug в html
 module.exports = function () {
 	return gulp
-		.src(['./src/pug/*.pug', './src/pug/pages/*.pug'])
-		.pipe(pug({ pretty: process.env.NODE_ENV === "development" }))
-		.pipe(gulp.dest("./build"))
+		.src(['./src/pug/pages/**/*.pug', ...excludePaths])
+		.pipe(pug({ pretty: process.env.NODE_ENV !== 'development' }))
+		.pipe(gulp.dest('./build'))
 		.pipe(browserSync.stream());
 };
